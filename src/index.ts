624a9e732f5161cf1672807c26285acb572820a0
Leandro Augusto <leandroaugusto470@gmail.com>
2023-08-14 11:49:17 -0300

    configuração e conexão do mongo com mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-﻿
+﻿import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+
+const dbUri = process.env.DB_URI as string;
+mongoose
+  .connect(dbUri)
+  .then(() => {
+    console.log('Database started');
+    const app = express();
+
+    app.use(express.json());
+    app.get('/', (_req, res) => {
+      return res.json('tudo certo');
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log('Server started');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
