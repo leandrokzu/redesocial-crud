37956d9d6b3e4bb077b837be61ac42d2a7fa0e4d
Leandro Augusto <leandroaugusto470@gmail.com>
2023-08-26 11:39:46 -0300

    crud usuários

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,33 @@
 import User from '../models/User';
 
 export class UserController {
+  async get(req: Request, res: Response) {
+    try {
+      const users = await User.find();
+
+      return res.json(users);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: 'Erro interno do servidor' });
+    }
+  }
+
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const user = await User.findById(id);
+
+      if (!user) {
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+      }
+
+      return res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: 'Erro interno do servidor' });
+    }
+  }
+
   async create(req: Request, res: Response) {
     const { name, email, username, photo, description } = req.body;
 
@@ -31,6 +58,66 @@ export class UserController {
     } catch (err) {
       console.log(err);
       return res.status(500).json({ message: 'Erro interno do servidor' });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    const { name, email, username, photo, description } = req.body;
+    const { id } = req.params;
+
+    try {
+      const user = await User.findById(id);
+
+      if (!user) {
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+      }
+
+      const existsUserEmailOrUsername = await User.findOne({
+        $or: [
+          { email, _id: { $ne: id } },
+          { username, _id: { $ne: id } },
+        ],
+      });
+
+      if (existsUserEmailOrUsername) {
+        return res
+          .status(400)
+          .json({ message: 'E-mail ou nome de usuário já existe!' });
+      }
+
+      await User.updateOne(
+        { _id: id },
+        {
+          name,
+          email,
+          username,
+          photo,
+          description,
+        }
+      );
+
+      return res.status(204).json();
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: 'Erro interno do servidor' });
+    }
+  }
+
+  async inactive(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const user = await User.findById(id);
+
+      if (!user) {
+        return res.status(404).json({ message: 'Usuário não encontrado' });
+      }
+
+      await User.updateOne({ isActive: false });
+
+      return res.status(204).json();
+    } catch (error) {
+      return res.status(500).json({ message: 'Erro interno do servidor!' });
     }
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,9 @@ import { UserController } from './controllers/UserController';
 const routes = Router();
 
 routes.post('/user', new UserController().create);
+routes.get('/user', new UserController().get);
+routes.get('/user/:id', new UserController().show);
+routes.put('/user/:id', new UserController().update);
+routes.patch('/user/:id', new UserController().inactive);
 
 export default routes;
